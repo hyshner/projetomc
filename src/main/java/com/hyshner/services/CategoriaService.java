86d346d81f04f5aa055ada2106e49a15ce383e6c
iuri ferreira <iuri.ferreira91@gmail.com>
2019-02-19 19:29:44 -0300

    paginação com parâmetros opcionais

--- src/main/java/com/hyshner/services/CategoriaService.java
+++ src/main/java/com/hyshner/services/CategoriaService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-
 import com.hyshner.domain.Categoria;
 import com.hyshner.repositories.CategoriaRepository;
 import com.hyshner.services.exeptions.ObjectNotFoudException;
@@ -51,5 +53,14 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(
+			Integer page, 
+			Integer linesPerPage, 
+			String orderBy,
+			String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
